4eebe3092565736ed8531e55d067aa96ae073360
Ross Cooper <ross.cooper@teknet.org.uk>
2017-07-14 17:15:51 +0100

    Set class is noe JsonSerializable

--- src/Models/Set.php
+++ src/Models/Set.php
@@ -7,7 +7,7 @@ namespace Pokemon\Models;
  *
  * @package Pokemon\Models
  */
-class Set extends Model
+class Set extends Model implements \JsonSerializable
 {
 
     /**
@@ -136,4 +136,17 @@ class Set extends Model
         $this->releaseDate = $releaseDate;
     }
 
+	/**
+	 * Specify data which should be serialized to JSON
+	 * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+	 * @return mixed data which can be serialized by <b>json_encode</b>,
+	 * which is a value of any type other than a resource.
+	 * @since 5.4.0
+	 */
+	function jsonSerialize() {
+		return [
+			'code' => $this->getCode(),
+			'name' => $this->getName()
+		];
+	}
 }
